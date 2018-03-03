92257628a2e903616d47195100d22f7fd720f628
Sanya Samaimak <sanya.sam@rmutr.ac.th>
2018-03-03 15:49:14 +0700

    Check space on main successed

--- app/src/main/java/slump/com/qrfirebase/fragment/MainFragment.java
+++ app/src/main/java/slump/com/qrfirebase/fragment/MainFragment.java
@@ -6,9 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import slump.com.qrfirebase.R;
+import slump.com.qrfirebase.utility.MainAlert;
 
 /**
  * Created by Slump on 03/03/2018.
@@ -23,7 +26,33 @@ public class MainFragment extends Fragment {
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
+
     }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String emailString = emailEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if ((emailString.isEmpty() == true) || (passwordString.isEmpty() == true)) {
+                    MainAlert mainAlert = new MainAlert(getActivity());
+                    mainAlert.normalDialog("Have space", "Please fill all blank");
+                } else {
+
+                }
+
+            }
+        });
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtNewRegister);
