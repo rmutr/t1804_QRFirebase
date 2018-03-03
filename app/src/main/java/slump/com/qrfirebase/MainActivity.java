47ff7a4074434f2aec510066bbdeccbc65268ac2
Sanya Samaimak <sanya.sam@rmutr.ac.th>
2018-03-03 11:38:32 +0700

    Design UI MainFragment

--- app/src/main/java/slump/com/qrfirebase/MainActivity.java
+++ app/src/main/java/slump/com/qrfirebase/MainActivity.java
@@ -3,11 +3,23 @@ package slump.com.qrfirebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import slump.com.qrfirebase.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
+
+    }   // Main Method
+
+}   // Main Class
---
+++ app/src/main/java/slump/com/qrfirebase/fragment/MainFragment.java
@@ -0,0 +1,25 @@
+package slump.com.qrfirebase.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import slump.com.qrfirebase.R;
+
+/**
+ * Created by Slump on 03/03/2018.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+
+}   // Main Class
